970ff9b2d27ade427478b06102f901179ee35e51
d2fong <d2fong@uwaterloo.ca>
2020-05-28 10:30:11 -0400

    remove promise-mysql to use better supported mysql2
    refs #52

--- src/server/db/download-data.js
+++ src/server/db/download-data.js
@@ -5,7 +5,7 @@ const downloadConnectivity = async (conn, opts) => {
   } = opts;
 
   // Only query individual cells.
-  const neurons = await conn.query('SELECT name FROM neurons').map((n) => `"${n.name}"`);
+  const [neurons, ] = await conn.query('SELECT name FROM neurons')[0].map((n) => `"${n.name}"`);
 
   const sql = `
     SELECT pre, post, type, synapses
@@ -16,7 +16,9 @@ const downloadConnectivity = async (conn, opts) => {
     ORDER BY pre, post, type
   `;
 
-  return conn.query(sql);
+  const [rows, ] = await conn.query(sql);
+
+  return rows;
 };
 
 module.exports = downloadConnectivity;
