74d7785b565c65bb3f339174cac0e41948af2aa1
d2fong <d2fong@uwaterloo.ca>
2020-06-02 11:26:32 -0400

    initial experiment to make tests less dependent on data values - create test utils helper functions to abstract common operations - delete unused function and associated test
    refs #20

---
+++ test/test-util.js
@@ -0,0 +1,16 @@
+
+
+const getDatasetTypes = ds => Array.from(ds.getDatabaseList());
+
+const getRandom = array => array[Math.floor(Math.random() * array.length)];
+
+const getRandomCell = ds => getRandom(ds.cells);
+
+const getRandomDatasetType = ds => getRandom(getDatasetTypes(ds));
+
+module.exports = {
+  getDatasetTypes,
+  getRandom,
+  getRandomCell,
+  getRandomDatasetType
+};
